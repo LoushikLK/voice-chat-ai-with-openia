919008955c3be1c11a4803441de824f5ff590af0
Loushik <loushikkumargiri@gmail.com>
2023-04-16 12:13:23 +0530

    changing functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Microphone } from "@/assets/Icons";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import SpeechRecognition, {
@@ -9,6 +10,11 @@ declare const window: any;
 export default function Home() {
   const [isListening, setIsListening] = useState(false);
   const [message, setMessage] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [transcriptMessage, setTranscriptMessage] = useState("");
+  const [fetching, setFetching] = useState(false);
+
+  const [sendMessage, setSendMessage] = useState(false);
 
   const {
     transcript,
@@ -27,7 +33,7 @@ export default function Home() {
           return;
         }
 
-        handleMessageFetch(transcript);
+        setTranscriptMessage(transcript);
       } catch (error) {
         console.log(error);
       }
@@ -36,6 +42,7 @@ export default function Home() {
 
   const handleMessageFetch = async (message: string) => {
     try {
+      setFetching(true);
       const response = await fetch(
         "https://chatbot-server-pqv8.onrender.com/",
         {
@@ -59,11 +66,19 @@ export default function Home() {
         );
 
         window.speechSynthesis.speak(speechService);
+        setAnswer(
+          data?.content || "Oops sorry i do not have any answer to this."
+        );
+
+        setMessage("");
       } else {
         alert("Speech synthesis not supported");
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      resetTranscript();
+      setFetching(false);
     }
   };
 
@@ -72,109 +87,83 @@ export default function Home() {
       <Head>
         <title>AI</title>
       </Head>
-      <main className=" bg-gray-900 min-h-screen flex items-center justify-center ">
+      <main className=" bg-gray-900 min-h-screen flex flex-col gap-4 text-white items-center justify-center ">
         <div className="w-fit h-fit shadow-xl  border-white rounded-full border-2 ">
-          <div className="w-fit p-16  ">
-            <svg
-              stroke="currentColor"
-              className="text-4xl"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 16 16"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
+          {fetching ? (
+            <div className="h-20 flex items-center justify-center p-24 w-20">
+              <p className="p-16">Thinking...</p>
+            </div>
+          ) : (
+            <div className="w-fit p-16  ">
+              <Microphone className="text-4xl text-white " />
+            </div>
+          )}
+        </div>
+
+        {transcriptMessage && (
+          <p className="font-medium tracking-wide container">
+            Question : {transcriptMessage}
+          </p>
+        )}
+        {answer && (
+          <p className="font-medium tracking-wide container">
+            Answer : {answer}
+          </p>
+        )}
+
+        {sendMessage ? (
+          <div className="flex flex-col w-full fixed bottom-0 items-center  p-4  gap-4">
+            <textarea
+              name=""
+              id=""
+              rows={5}
+              className="w-full max-w-5xl p-2 rounded-lg text-gray-700 "
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            <div className="flex items-center gap-4">
+              <button
+                className="bg-blue-500 px-4 py-2 rounded-full hover:bg-blue-700 "
+                onClick={() => {
+                  handleMessageFetch(message);
+                  setTranscriptMessage(message);
+                  setAnswer("");
+                }}
+              >
+                Send Message
+              </button>
+              <button
+                className="bg-gray-500 px-4 py-2 rounded-full hover:bg-gray-700 "
+                onClick={() => setSendMessage(false)}
+              >
+                Switch to voice
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="flex flex-col w-full fixed bottom-0 items-center  p-4  gap-4">
+            <button
+              className=" bg-blue-500 p-4 rounded-full cursor-pointer  hover:bg-blue-700   "
+              onMouseDown={() => {
+                setIsListening(true);
+                SpeechRecognition.startListening();
+              }}
+              onMouseUp={() => {
+                setIsListening(false);
+                SpeechRecognition.abortListening();
+                handleMessageFetch(transcriptMessage);
+              }}
             >
-              <path
-                fillRule="evenodd"
-                d="M3.5 6.5A.5.5 0 014 7v1a4 4 0 008 0V7a.5.5 0 011 0v1a5 5 0 01-4.5 4.975V15h3a.5.5 0 010 1h-7a.5.5 0 010-1h3v-2.025A5 5 0 013 8V7a.5.5 0 01.5-.5z"
-                clipRule="evenodd"
-              ></path>
-              <path
-                fillRule="evenodd"
-                d="M10 8V3a2 2 0 10-4 0v5a2 2 0 104 0zM8 0a3 3 0 00-3 3v5a3 3 0 006 0V3a3 3 0 00-3-3z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+              <Microphone className="text-xl" />
+            </button>
+            <button
+              className="bg-gray-600 px-4 py-1 rounded-full hover:bg-gray-700 "
+              onClick={() => setSendMessage(true)}
+            >
+              Type
+            </button>
           </div>
-        </div>
-        <div className="w-full fixed bottom-0 flex  p-4  gap-4">
-          <textarea
-            name=""
-            id=""
-            className="w-full rounded-md text-gray-900 p-4 "
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            rows={5}
-          ></textarea>
-          <div className="flex flex-col h-full justify-between gap-4 ">
-            {!message?.trim()?.length ? (
-              <div
-                className="w-fit  bg-blue-500 px-8 py-3 rounded-md shadow-xl cursor-pointer "
-                onClick={() => {
-                  SpeechRecognition.startListening({
-                    continuous: true,
-                    language: "en-IN",
-                  });
-                  setIsListening(true);
-                }}
-              >
-                <svg
-                  stroke="currentColor"
-                  className="text-xl"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 16 16"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3.5 6.5A.5.5 0 014 7v1a4 4 0 008 0V7a.5.5 0 011 0v1a5 5 0 01-4.5 4.975V15h3a.5.5 0 010 1h-7a.5.5 0 010-1h3v-2.025A5 5 0 013 8V7a.5.5 0 01.5-.5z"
-                    clipRule="evenodd"
-                  ></path>
-                  <path
-                    fillRule="evenodd"
-                    d="M10 8V3a2 2 0 10-4 0v5a2 2 0 104 0zM8 0a3 3 0 00-3 3v5a3 3 0 006 0V3a3 3 0 00-3-3z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </div>
-            ) : (
-              <div
-                className="w-fit  bg-blue-500 px-8 py-3 rounded-md shadow-xl cursor-pointer "
-                onClick={() => handleMessageFetch(message)}
-              >
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 1024 1024"
-                  version="1.1"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <defs></defs>
-                  <path d="M931.4 498.9L94.9 79.5c-3.4-1.7-7.3-2.1-11-1.2-8.5 2.1-13.8 10.7-11.7 19.3l86.2 352.2c1.3 5.3 5.2 9.6 10.4 11.3l147.7 50.7-147.6 50.7c-5.2 1.8-9.1 6-10.3 11.3L72.2 926.5c-0.9 3.7-0.5 7.6 1.2 10.9 3.9 7.9 13.5 11.1 21.5 7.2l836.5-417c3.1-1.5 5.6-4.1 7.2-7.1 3.9-8 0.7-17.6-7.2-21.6zM170.8 826.3l50.3-205.6 295.2-101.3c2.3-0.8 4.2-2.6 5-5 1.4-4.2-0.8-8.7-5-10.2L221.1 403 171 198.2l628 314.9-628.2 313.2z"></path>
-                </svg>
-              </div>
-            )}
-            {isListening && (
-              <div
-                className="font-medium bg-red-500 text-white cursor-pointer rounded-md py-2 px-4 tracking-wide text-md"
-                onClick={() => {
-                  resetTranscript();
-                  SpeechRecognition.stopListening();
-                  setIsListening(false);
-                }}
-              >
-                Stop
-              </div>
-            )}
-          </div>
-        </div>
+        )}
       </main>
     </>
   );
