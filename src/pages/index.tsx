3d91608bf585312f9d620f0bf0730fa9465abbb2
Loushik <loushikkumargiri@gmail.com>
2023-05-07 12:52:45 +0530

    complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
-import { Microphone } from "@/assets/Icons";
+"use client";
+
+import { Microphone, Send } from "@/assets/Icons";
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import { v4 as uuidV4 } from "uuid";
+
+const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(
+  "869f2f3b-b56c-43d3-9c9c-d5d8dc55cc22"
+);
+SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
 
 declare const window: any;
 
-export default function Home() {
-  const [isListening, setIsListening] = useState(false);
-  const [message, setMessage] = useState("");
-  const [answer, setAnswer] = useState("");
-  const [transcriptMessage, setTranscriptMessage] = useState("");
-  const [fetching, setFetching] = useState(false);
+type ChatMessage = {
+  id: string;
+  question: string;
+  answer: string;
+};
 
-  const [sendMessage, setSendMessage] = useState(false);
+export default function Home() {
+  const [chatMessage, setChatMessage] = useState<ChatMessage[]>([]);
+  const [question, setQuestion] = useState("");
+
+  const isChanging = useRef(false);
 
   const {
     transcript,
@@ -23,62 +35,151 @@ export default function Home() {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
+  const handleStartListening = () => {
+    resetTranscript();
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  };
+
   useEffect(() => {
-    if (!isListening && !transcript?.length) return;
+    if (!transcript?.length) return;
 
-    (async () => {
-      try {
-        if (!browserSupportsSpeechRecognition) {
-          alert("Browser does not support speech recognition");
-          return;
+    if (transcript.includes("cancel this question")) {
+      resetTranscript();
+    }
+
+    if (isChanging.current) {
+      setChatMessage((prev) =>
+        prev.filter((item, index) => index !== prev.length - 1)
+      );
+    }
+
+    isChanging.current = true;
+
+    setChatMessage((prev) => {
+      return [
+        ...prev,
+        {
+          id: uuidV4(),
+          question: transcript,
+          answer: "",
+        },
+      ];
+    });
+
+    setQuestion(transcript);
+  }, [transcript]);
+
+  const handleButtonRemoveHold = async () => {
+    SpeechRecognition.stopListening();
+
+    let uuid = uuidV4();
+
+    setChatMessage((prev) => {
+      return [
+        ...prev,
+        {
+          id: uuid,
+          question: question,
+          answer: "",
+        },
+      ];
+    });
+
+    const response = await handleFetchData(question);
+
+    setQuestion("");
+
+    setChatMessage((prev) => {
+      return prev?.map((item) => {
+        if (item?.id === uuid) {
+          return {
+            ...item,
+            answer: response || "Oops sorry i do not have any answer to this.",
+          };
         }
+        return item;
+      });
+    });
 
-        setTranscriptMessage(transcript);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, [isListening, transcript]);
+    handleSpeechSynthesis(response);
+  };
 
-  const handleMessageFetch = async (message: string) => {
+  const handleSendMessage = async () => {
     try {
-      setFetching(true);
+      let uuid = uuidV4();
+
+      setChatMessage((prev) => {
+        return [
+          ...prev,
+          {
+            id: uuid,
+            question: question,
+            answer: "",
+          },
+        ];
+      });
+
+      const response = await handleFetchData(question);
+
+      setQuestion("");
+
+      setChatMessage((prev) => {
+        return prev?.map((item) => {
+          if (item?.id === uuid) {
+            return {
+              ...item,
+              answer:
+                response || "Oops sorry i do not have any answer to this.",
+            };
+          }
+          return item;
+        });
+      });
+
+      handleSpeechSynthesis(response);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const handleFetchData: (arg: string) => Promise<string> = async (
+    question: string
+  ) => {
+    try {
       const response = await fetch(
         "https://chatbot-server-pqv8.onrender.com/",
+        // "http://localhost:8000/",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            question: message,
+            question: question,
           }),
         }
       );
 
       const data = await response.json();
 
-      if ("speechSynthesis" in window) {
-        window.speechSynthesis.cancel();
-
-        const speechService = new SpeechSynthesisUtterance(
-          data?.content || "Oops sorry i do not have any answer to this."
-        );
-
-        window.speechSynthesis.speak(speechService);
-        setAnswer(
-          data?.content || "Oops sorry i do not have any answer to this."
-        );
-
-        setMessage("");
-      } else {
-        alert("Speech synthesis not supported");
-      }
+      return data?.content || "Oops sorry i do not have any answer to this.";
     } catch (error) {
-      console.log(error);
-    } finally {
-      resetTranscript();
-      setFetching(false);
+      alert(error);
+      return "Something went wrong!";
+    }
+  };
+
+  const handleSpeechSynthesis = (message: string) => {
+    if ("speechSynthesis" in window) {
+      window.speechSynthesis.cancel();
+
+      const speechService = new SpeechSynthesisUtterance(
+        message || "Oops sorry i do not have any answer to this."
+      );
+
+      window.speechSynthesis.speak(speechService);
+    } else {
+      alert("Speech synthesis not supported");
     }
   };
 
@@ -87,83 +188,84 @@ export default function Home() {
       <Head>
         <title>AI</title>
       </Head>
-      <main className=" bg-gray-900 min-h-screen flex flex-col gap-4 text-white items-center justify-center ">
-        <div className="w-fit h-fit shadow-xl  border-white rounded-full border-2 ">
-          {fetching ? (
-            <div className="h-20 flex items-center justify-center p-24 w-20">
-              <p className="p-16">Thinking...</p>
-            </div>
-          ) : (
-            <div className="w-fit p-16  ">
-              <Microphone className="text-4xl text-white " />
-            </div>
-          )}
-        </div>
+      <main className=" bg-gray-900 min-h-screen flex flex-col gap-4  text-white  ">
+        <h1 className="w-full text-center p-4 text-4xl text-teal-500 font-medium tracking-wide ">
+          AI Chat Room
+        </h1>
+        <div
+          className={`w-full flex flex-col min-h-[90vh] container mx-auto p-4  px-4 pt-4 relative `}
+        >
+          <div className="flex flex-col h-max overflow-y-auto left-menu-scroll pb-20 ">
+            {chatMessage?.map((item) => (
+              <>
+                {item?.question?.length && (
+                  <div
+                    className="relative w-full left-0 text-sm  py-4 flex items-center justify-end  "
+                    key={item?.id + item?.question}
+                  >
+                    <div className="w-full flex flex-col items-end mr-4  gap-2">
+                      <span className="max-w-[90%] md:max-w-[70%] w-fit   text-sm text-black bg-gray-100 rounded-b-2xl rounded-l-2xl shadow-lg font-medium tracking-wide py-2 px-4">
+                        {item?.question}
+                      </span>
+                      <span className="flex items-center gap-2">
+                        <small className="text-gray-500 tracking-wide"></small>
+                      </span>
+                    </div>
+                  </div>
+                )}
 
-        {transcriptMessage && (
-          <p className="font-medium tracking-wide container">
-            Question : {transcriptMessage}
-          </p>
-        )}
-        {answer && (
-          <p className="font-medium tracking-wide container">
-            Answer : {answer}
-          </p>
-        )}
+                {item?.answer?.length ? (
+                  <div
+                    className="relative w-full py-4 left-0  gap-4 flex items-start justify-start"
+                    key={item?.id}
+                  >
+                    <div className="w-full flex flex-col gap-1">
+                      <div className="flex items-center gap-2">
+                        <span
+                          className="max-w-[90%] md:max-w-[70%] w-fit py-2 px-4   text-sm text-white 
+                     bg-teal-500 font-medium tracking-wide rounded-b-2xl rounded-r-2xl"
+                        >
+                          {item?.answer}
+                        </span>
+                      </div>
+                      <small className="text-gray-500 tracking-wide"></small>
+                    </div>
+                  </div>
+                ) : (
+                  <>Thinking...</>
+                )}
+              </>
+            ))}
+          </div>
 
-        {sendMessage ? (
-          <div className="flex flex-col w-full fixed bottom-0 items-center  p-4  gap-4">
-            <textarea
-              name=""
-              id=""
-              rows={5}
-              className="w-full max-w-5xl p-2 rounded-lg text-gray-700 "
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-            <div className="flex items-center gap-4">
-              <button
-                className="bg-blue-500 px-4 py-2 rounded-full hover:bg-blue-700 "
-                onClick={() => {
-                  handleMessageFetch(message);
-                  setTranscriptMessage(message);
-                  setAnswer("");
+          <div className="w-full h-fit flex absolute bottom-0 left-0 items-center justify-between gap-4 bg-white  dark:bg-gray-900 p-4    z-10  ">
+            <div className="flex w-full items-center border p-2 rounded-full ">
+              <input
+                type="text"
+                placeholder="Type a message..."
+                className="w-full focus:outline-none bg-transparent text-black dark:text-white pl-4 "
+                value={question}
+                onChange={(e: any) => {
+                  setQuestion(e.target.value);
                 }}
-              >
-                Send Message
-              </button>
-              <button
-                className="bg-gray-500 px-4 py-2 rounded-full hover:bg-gray-700 "
-                onClick={() => setSendMessage(false)}
-              >
-                Switch to voice
-              </button>
+              />
             </div>
-          </div>
-        ) : (
-          <div className="flex flex-col w-full fixed bottom-0 items-center  p-4  gap-4">
             <button
-              className=" bg-blue-500 p-4 rounded-full cursor-pointer  hover:bg-blue-700   "
-              onMouseDown={() => {
-                setIsListening(true);
-                SpeechRecognition.startListening();
-              }}
-              onMouseUp={() => {
-                setIsListening(false);
-                SpeechRecognition.abortListening();
-                handleMessageFetch(transcriptMessage);
-              }}
+              className="bg-gray-200/20 rounded-full h-10 w-10 flex items-center justify-center cursor-pointer  "
+              onClick={handleSendMessage}
             >
-              <Microphone className="text-xl" />
+              <Send />
             </button>
             <button
-              className="bg-gray-600 px-4 py-1 rounded-full hover:bg-gray-700 "
-              onClick={() => setSendMessage(true)}
+              className="bg-gray-200/20 rounded-full h-10 w-10 flex items-center justify-center cursor-pointer  "
+              onMouseDown={handleStartListening}
+              onPointerDown={handleStartListening}
+              onMouseUp={handleButtonRemoveHold}
             >
-              Type
+              <Microphone />
             </button>
           </div>
-        )}
+        </div>
       </main>
     </>
   );
